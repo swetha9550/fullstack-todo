b188fde6ad2a0391b2ce8d8e0a8433014524f5f2
swetha9550 <pyllaswethareddy@gmail.com>
2026-02-18 08:48:42 +0530

    Enable API functions for todo management
    Uncommented API functions for todo operations.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,4 @@
-// import axios from "axios";
 
-// const API = "http://localhost:8080/api/todos";
-
-// export const getTodos = () => axios.get(API);
-// export const addTodo = (todo) => axios.post(API, todo);
-// export const deleteTodo = (id) => axios.delete(`${API}/${id}`);
-// export const updateTodo = (id, todo) => axios.put(`${API}/${id}`, todo);
 import axios from "axios";
 
 const API = "http://localhost:8080/api/todos";
